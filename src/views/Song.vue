9f961832da8566a10e022b89149b9e8955ece915
AnomicAphasia <travis.briscoe@gmail.com>
2021-07-14 13:55:49 -0400

    Section 16 Start (upgraded some yarn packages)

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -1,14 +1,14 @@
 <template>
   <main>
-      <section class="w-full mb-8 py-14 text-center text-white relative">
+    <section class="w-full mb-8 py-14 text-center text-white relative">
       <div class="absolute inset-0 w-full h-full box-border bg-contain music-bg"
         style="background-image: url(/assets/img/song-header.png)">
       </div>
       <div class="container mx-auto flex items-center">
         <!-- Play/Pause Button -->
         <button type="button" class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full
-          focus:outline-none">
-          <i class="fas fa-play"></i>
+          focus:outline-none" @click.prevent="newSong(song)">
+          <i class="fas" :class="{ 'fa-play': !playing, 'fa-pause': playing }"></i>
         </button>
         <div class="z-50 text-left ml-8">
           <!-- Song Info -->
@@ -18,11 +18,11 @@
       </div>
     </section>
     <!-- Form -->
-    <section class="container mx-auto mt-6">
+    <section class="container mx-auto mt-6" id="comments">
       <div class="bg-white rounded border border-gray-200 relative flex flex-col">
         <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
           <!-- Comment Count -->
-          <span class="card-title">Comments (15)</span>
+          <span class="card-title">Comments ({{ song.comment_count }})</span>
           <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
         </div>
         <div class="p-6">
@@ -31,7 +31,8 @@
             :class="comment_alert_variant">
             {{ comment_alert_message }}
           </div>
-          <vee-form :validation-schema="schema" @submit="addComment">
+          <vee-form :validation-schema="schema" @submit="addComment"
+            v-if="userLoggedIn">
             <vee-field as="textarea" name="comment"
               class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                 duration-500 focus:outline-none focus:border-black rounded mb-4"
@@ -42,7 +43,7 @@
             </button>
           </vee-form>
           <!-- Sort Comments -->
-          <select
+          <select v-model="sort"
             class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition
             duration-500 focus:outline-none focus:border-black rounded">
             <option value="1">Latest</option>
@@ -53,17 +54,15 @@
     </section>
     <!-- Comments -->
     <ul class="container mx-auto">
-      <li class="p-6 bg-gray-50 border border-gray-200">
+      <li class="p-6 bg-gray-50 border border-gray-200" v-for="comment in sortedComments"
+        :key="comment.docID">
         <!-- Comment Author -->
         <div class="mb-5">
-          <div class="font-bold">Elaine Dreyfuss</div>
-          <time>5 mins ago</time>
+          <div class="font-bold">{{ comment.name }}</div>
+          <time>{{ comment.datePosted }}</time>
         </div>
 
-        <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium der doloremque laudantium.
-        </p>
+        <p>{{ comment.content }}</p>
       </li>
     </ul>
   </main>
@@ -71,6 +70,7 @@
 
 <script>
 import { songsCollection, auth, commentsCollection } from '@/includes/firebase';
+import { mapState, mapActions, mapGetters } from 'vuex';
 
 export default {
   data() {
@@ -83,7 +83,22 @@ export default {
       comment_show_alert: false,
       comment_alert_variant: 'bg-blue-500',
       comment_alert_message: 'Please wait! Your comment is being submitted',
+      comments: [],
+      sort: 1,
     };
+  },
+  computed: {
+    ...mapState(['userLoggedIn']),
+    ...mapGetters(['playing']),
+    sortedComments() {
+      return this.comments.slice().sort((a, b) => {
+        if (this.sort === '1') {
+          return new Date(b.datePosted) - new Date(a.datePosted);
+        }
+
+        return new Date(a.datePosted) - new Date(b.datePosted);
+      });
+    },
   },
   async created() {
     const docSnapShot = await songsCollection.doc(this.$route.params.id).get();
@@ -93,9 +108,15 @@ export default {
       return;
     }
 
+    const { sort } = this.$route.query;
+
+    this.sort = sort === '1' || sort === '2' ? sort : '1';
+
     this.song = docSnapShot.data();
+    this.getComments();
   },
   methods: {
+    ...mapActions(['newSong']),
     async addComment(values, { resetForm }) {
       this.comment_in_submission = true;
       this.comment_show_alert = true;
@@ -112,11 +133,45 @@ export default {
 
       await commentsCollection.add(comment);
 
+      this.song.comment_count += 1;
+      await songsCollection.doc(this.$route.params.id).update({
+        comment_count: this.song.comment_count,
+      });
+
+      this.getComments();
+
       this.comment_in_submission = false;
       this.comment_alert_variant = 'bg-green-500';
       this.comment_alert_message = 'Comment added!';
 
       resetForm();
+    },
+    async getComments() {
+      const snapshots = await commentsCollection.where(
+        'sid', '==', this.$route.params.id,
+      ).get();
+
+      this.comments = [];
+
+      snapshots.forEach((doc) => {
+        this.comments.push({
+          docID: doc.id,
+          ...doc.data(),
+        });
+      });
+    },
+  },
+  watch: {
+    sort(newVal) {
+      if (newVal === this.$route.query.sort) {
+        return;
+      }
+
+      this.$router.push({
+        query: {
+          sort: newVal,
+        },
+      });
     },
   },
 };
